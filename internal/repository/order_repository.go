1edd97501e7bd0d11ea14810ff788207172e8bb4
yaninyzwitty <ianmwa143@gmail.com>
2024-10-26 14:53:02 +0300

    added code for order repository

--- internal/repository/order_repository.go
+++ internal/repository/order_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/gocql/gocql"
 	"github.com/yaninyzwitty/golang-rest-grpc-proj/internal/models"
@@ -23,16 +24,51 @@ func NewOrderRepository(session *gocql.Session) OrderRepository {
 }
 
 func (r *orderRepository) CreateOrder(ctx context.Context, order models.Order) (*models.Order, error) {
-	return &models.Order{}, nil
+	query := `INSERT INTO ecommerce.orders (id, product_id, quantity, customer_id, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?)`
+	err := r.session.Query(query, order.ID, order.ProductID, order.Quantity, order.CustomerID, order.CreatedAt, order.UpdatedAt).Exec()
+	if err != nil {
+		return nil, fmt.Errorf("failed to insert data: %v", err)
+	}
+	return &order, nil
 }
 
 func (r *orderRepository) DeleteOrder(ctx context.Context, orderId gocql.UUID) (bool, error) {
-	return false, nil
+	query := `DELETE FROM eccomerce.orders WHERE id = ?`
+	err := r.session.Query(query, orderId).Exec()
+	if err != nil {
+		return false, fmt.Errorf("failed to delete the order: %v", err)
+	}
+	return true, nil
 }
 
 func (r *orderRepository) UpdateOrder(ctx context.Context, order models.Order, orderId gocql.UUID) (*models.Order, error) {
-	return &models.Order{}, nil
+	query := `UPDATE eccomerce.orders SET quantity = ?, updated_at = ? WHERE id = ?`
+	if err := r.session.Query(query, order.Quantity, order.UpdatedAt, orderId).Exec(); err != nil {
+		return nil, fmt.Errorf("failed to update the orders: %v", err)
+	}
+	return &order, nil
 }
 func (r *orderRepository) GetOrder(ctx context.Context, orderId gocql.UUID) (*models.Order, error) {
-	return &models.Order{}, nil
+	var order models.Order
+	query := `SELECT id, product_id, quantity, customer_id, updated_at, created_at FROM orders WHERE id = ?`
+
+	// Execute the query and scan the result into the order variable
+	err := r.session.Query(query, orderId).Consistency(gocql.One).Scan(
+		&order.ID,
+		&order.ProductID,
+		&order.Quantity,
+		&order.CustomerID,
+		&order.UpdatedAt,
+		&order.CreatedAt,
+	)
+
+	if err != nil {
+		if err == gocql.ErrNotFound {
+			// Return nil order and nil error for not found case
+			return nil, nil
+		}
+		// Return an error for any other issues
+		return nil, err
+	}
+	return &order, nil // Return the populated order
 }
